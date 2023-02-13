a657ee07f4e68f0c99beb2dff5a7005ccba74b18
elyakan <elyakan@gmail.com>
2023-02-13 10:24:56 -0300

    Update ClaveunicaController.java

--- src/main/java/cl/fonasa/claveunica/controllers/ClaveunicaController.java
+++ src/main/java/cl/fonasa/claveunica/controllers/ClaveunicaController.java
@@ -1,11 +1,15 @@
 package cl.fonasa.claveunica.controllers;
 
 import io.vertx.core.json.JsonObject;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -28,8 +32,8 @@ public class ClaveunicaController {
     // Estas variables contienen los datos de autenticacion del aplicativo con clave unica
     //produccion
 /*
-    private String clientId="55f13050939c44688cb1bb852199b805";   // Cliente Id entregado por claveunica
-    private String clientSecret="18b7e761e2594d02812a7bc5ae830999"; // Client Secret entregado por claveunica
+    private String clientId="sssssssss";   // Cliente Id entregado por claveunica
+    private String clientSecret="aaaaaaaaaa"; // Client Secret entregado por claveunica
 */
 
     //desarrollo sanbox
@@ -65,58 +69,65 @@ public class ClaveunicaController {
             , consumes = "application/x-www-form-urlencoded;charset=UTF-8"
     )
     @ResponseBody
-    public String token(@RequestParam Map<String, String> data) throws IOException {
+    public String token(@RequestParam Map<String, String> data) throws IOException  {
         // data array (0.0)-->(content, value)-->representa los valores en headers
-        String accessToken = "";
-        String code = data.get("code");
-        String state = data.get("state");
-        String localUri= data.get("redirect_uri");
+        try {
+            String accessToken = "";
+            String code = data.get("code");
+            String state = data.get("state");
+            String localUri = data.get("redirect_uri");
 
-        String query = "client_id=" +clientId;
-        query += "&";
-        query += "client_secret=" + clientSecret;
-        query += "&";
-        query += "redirect_uri=" + localUri;
-        query += "&";
-        query += "grant_type=authorization_code";
-        query += "&";
-        query += "code=" + code;
-        query += "&";
-        query += "state=" + state;
+            String query = "client_id=" + clientId;
+            query += "&";
+            query += "client_secret=" + clientSecret;
+            query += "&";
+            query += "redirect_uri=" + localUri;
+            query += "&";
+            query += "grant_type=authorization_code";
+            query += "&";
+            query += "code=" + code;
+            query += "&";
+            query += "state=" + state;
 
-        URL myUrl = new URL( urlClaveUnica + "openid/token/");
-        HttpsURLConnection con = (HttpsURLConnection) myUrl.openConnection();
-        con.setRequestMethod("POST");
+            String urlstr=urlClaveUnica + "openid/token/";
+            URL myUrl = new URL(null, urlstr, new sun.net.www.protocol.https.Handler());
+            HttpsURLConnection con = (HttpsURLConnection) myUrl.openConnection();
 
-        con.setRequestProperty("Content-length", String.valueOf(query.length()));
-        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0;Windows98;DigExt)");
-        con.setDoOutput(true);
-        con.setDoInput(true);
+            con.setRequestMethod("POST");
 
-        DataOutputStream output = new DataOutputStream(con.getOutputStream());
-        output.write(query.getBytes(StandardCharsets.UTF_8));
-        output.flush();
-        output.close();
+            con.setRequestProperty("Content-length", String.valueOf(query.length()));
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            con.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0;Windows98;DigExt)");
+            con.setDoOutput(true);
+            con.setDoInput(true);
 
-        InputStream is;
-        if (con.getResponseCode() >= 400) {
-            is = con.getErrorStream();
-        } else {
-            is = con.getInputStream();
+            DataOutputStream output = new DataOutputStream(con.getOutputStream());
+            output.write(query.getBytes(StandardCharsets.UTF_8));
+            output.flush();
+            output.close();
+
+            InputStream is;
+            if (con.getResponseCode() >= 400) {
+                is = con.getErrorStream();
+            } else {
+                is = con.getInputStream();
+            }
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(is));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            for (inputLine = input.readLine(); inputLine != null; inputLine = input.readLine()) {
+                response.append(inputLine);
+            }
+            input.close();
+
+            JsonObject json = new JsonObject(response.toString());
+            accessToken = json.getString("access_token");
+            return accessToken;
         }
-
-        BufferedReader input = new BufferedReader(new InputStreamReader(is));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        for (inputLine = input.readLine(); inputLine != null; inputLine = input.readLine()) {
-            response.append(inputLine);
+        catch (Exception  e){
+            return(e.toString());
         }
-        input.close();
-
-        JsonObject json = new JsonObject(response.toString());
-        accessToken = json.getString("access_token");
-        return accessToken ;
     }
 
     @POST
@@ -125,22 +136,34 @@ public class ClaveunicaController {
     @PostMapping(value="/info")
     public ResponseEntity info(@RequestHeader("Authorization") String code) {
 
+        try {
+            ResponseEntity<String> response = null;
 
-        ResponseEntity<String> response = null;
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.add("Authorization", code);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+            headers.add("Authorization", code);
 
-        HttpEntity<String> request = new HttpEntity<>(headers);
+            HttpEntity<String> request = new HttpEntity<>(headers);
 
-        String accessTokenUrl = urlClaveUnica + "openid/userinfo/";
-        accessTokenUrl += "?code=" + code;
-        accessTokenUrl += "&grant_type=authorization_code";
-        accessTokenUrl += "&redirect_uri="+ redirectUri;
+            String accessTokenUrl = urlClaveUnica + "openid/userinfo/";
+            accessTokenUrl += "?code=" + code;
+            accessTokenUrl += "&grant_type=authorization_code";
+            accessTokenUrl += "&redirect_uri=" + redirectUri;
 
-        response = restTemplate.exchange(accessTokenUrl, HttpMethod.POST, request, String.class);
+            SSLContext context = SSLContext.getInstance("TLSv1.2");
+            context.init(null, null, null);
 
-        return response;
+            CloseableHttpClient httpClient = HttpClientBuilder.create().setSslcontext(context).build();
+            HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+            RestTemplate restTemplate = new RestTemplate(factory);
+
+            response = restTemplate.exchange(accessTokenUrl, HttpMethod.POST, request, String.class);
+
+            return response;
+        }
+        catch (Exception e){
+             return new ResponseEntity<String>(e.toString(), HttpStatus.NOT_IMPLEMENTED);
+
+        }
     }
 }
